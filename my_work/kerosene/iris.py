36b6809fe7b76acac5247fcfca50642c74663825
ozcell <ozsel@ozsel-mac.local>
2016-02-16 05:28:35 -0500

    Kerosene examples are added

---
+++ my_work/kerosene/iris.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from kerosene.datasets import iris
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.utils import np_utils
+
+'''
+    Train something simple on the classic iris dataset.
+
+    This version can get to 63.16% test accuracy after 8 epochs,
+    and it remains there for future epochs, though the train loss
+    continues to fall for all 12 epochs.
+    <1 second per epoch, even on your CPU.
+'''
+
+batch_size = 1
+nb_classes = 3
+nb_epoch = 12
+
+# the data, shuffled and split between tran and test sets
+(X_all, y_all), = iris.load_data()
+
+# print shape of data while model is building
+print("{0} samples in all, {1} columns".format(*X_all.shape))
+_, input_dim = X_all.shape
+
+# convert class vectors to binary class matrices
+Y_all = np_utils.to_categorical(y_all, nb_classes)
+
+model = Sequential()
+model.add(Dense(input_dim=input_dim, output_dim=nb_classes, init='glorot_uniform'))
+model.add(Activation('softmax'))
+model.compile(loss='mean_squared_error', optimizer='rmsprop')                    
+
+model.fit(X_all, Y_all, batch_size=batch_size, nb_epoch=nb_epoch, show_accuracy=True, validation_split=0.25, verbose=1)
---
+++ my_work/kerosene/mnist.py
@@ -0,0 +1,68 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from kerosene.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+'''
+    Train a simple convnet on the MNIST dataset.
+
+    http://yann.lecun.com/exdb/mnist/
+
+    This version can get to 99.10% test accuracy after 12 epochs.
+    8 seconds per epoch on a GeForce GTX 680 GPU.
+'''
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 12
+
+# the data, shuffled and split between tran and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# print shape of data while model is building
+print("{1} train samples, {2} channel{0}, {3}x{4}".format("" if X_train.shape[1] == 1 else "s", *X_train.shape))
+print("{1}  test samples, {2} channel{0}, {3}x{4}".format("" if X_test.shape[1] == 1 else "s", *X_test.shape))
+
+# input image dimensions
+_, _, img_rows, img_cols = X_train.shape
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='valid',
+                        input_shape=(1, img_rows, img_cols)))
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
