113af0679c9af12c3ccecc0ef5be041d4c41dda3
ozcell <ozsel@ozsel-mac.local>
2016-03-14 23:41:48 -0400

    SCOL Dropout Application

--- my_work/mnist_mlp2.py
+++ my_work/mnist_mlp2.py
@@ -54,7 +54,7 @@ model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dropout(0.2))
 model.add(Activation('softmax'))
-model.add(LinDense(10, dropout_rate=0.2))
+model.add(LinDense(10))
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.95, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
