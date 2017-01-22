cb6d69c86e02ffd49647232a5be58290317637da
ozcell <ozsel@ozsel-mac.local>
2017-01-21 23:42:15 -0500

    Keras update issues

--- keras/layers/scol.py
+++ keras/layers/scol.py
@@ -4,28 +4,45 @@ from __future__ import division
 
 import numpy as np
 
-from collections import OrderedDict
 import copy
-from six.moves import zip
+import inspect
+import types as python_types
+import warnings
 
 from .. import backend as K
-from .. import activations, initializations, initializations_scol, regularizers, constraints
-from ..regularizers import ActivityRegularizer
+from .. import activations
+from .. import initializations
+from .. import initializations_scol
+from .. import regularizers
+from .. import constraints
+from ..engine import InputSpec
 from ..engine import Layer
-
-import marshal
-import types
-import sys
+from ..engine import Merge
+from ..utils.generic_utils import func_dump
+from ..utils.generic_utils import func_load
+from ..utils.generic_utils import get_from_module
 
 
 class LinDense(Layer):
-    '''Just your regular fully connected NN layer.
+    """Just your regular densely-connected NN layer.
 
-    # Input shape
-        2D tensor with shape: `(nb_samples, input_dim)`.
+    # Example
 
-    # Output shape
-        2D tensor with shape: `(nb_samples, output_dim)`.
+    ```python
+        # as first layer in a sequential model:
+        model = Sequential()
+        model.add(Dense(32, input_dim=16))
+        # now the model will take as input arrays of shape (*, 16)
+        # and output arrays of shape (*, 32)
+
+        # this is equivalent to the above:
+        model = Sequential()
+        model.add(Dense(32, input_shape=(16,)))
+
+        # after the first layer, you don't need to specify
+        # the size of the input anymore:
+        model.add(Dense(32))
+    ```
 
     # Arguments
         output_dim: int > 0.
@@ -39,8 +56,9 @@ class LinDense(Layer):
             or alternatively, elementwise Theano function.
             If you don't specify anything, no activation is applied
             (ie. "linear" activation: a(x) = x).
-        weights: list of numpy arrays to set as initial weights.
-            The list should have 1 element, of shape `(input_dim, output_dim)`.
+        weights: list of Numpy arrays to set as initial weights.
+            The list should have 2 elements, of shape `(input_dim, output_dim)`
+            and (output_dim,) for weights and biases respectively.
         W_regularizer: instance of [WeightRegularizer](../regularizers.md)
             (eg. L1 or L2 regularization), applied to the main weights matrix.
         b_regularizer: instance of [WeightRegularizer](../regularizers.md),
@@ -51,129 +69,99 @@ class LinDense(Layer):
             (eg. maxnorm, nonneg), applied to the main weights matrix.
         b_constraint: instance of the [constraints](../constraints.md) module,
             applied to the bias.
-        input_dim: dimensionality of the input (integer).
-            This argument (or alternatively, the keyword argument `input_shape`)
+        bias: whether to include a bias
+            (i.e. make the layer affine rather than linear).
+        input_dim: dimensionality of the input (integer). This argument
+            (or alternatively, the keyword argument `input_shape`)
             is required when using this layer as the first layer in a model.
-    '''
-    input_ndim = 2
 
-    def __init__(self, output_dim, init='identity_vstacked', activation='linear', weights=None,
-                 input_dim=None, **kwargs):
+    # Input shape
+        nD tensor with shape: `(nb_samples, ..., input_dim)`.
+        The most common situation would be
+        a 2D input with shape `(nb_samples, input_dim)`.
 
-        self.init = initializations_scol.get(init)
+    # Output shape
+        nD tensor with shape: `(nb_samples, ..., output_dim)`.
+        For instance, for a 2D input with shape `(nb_samples, input_dim)`,
+        the output would have shape `(nb_samples, output_dim)`.
+    """
+
+    def __init__(self, output_dim, init='identity_vstacked',
+                 activation='linear', weights=None,
+                 W_regularizer=None, b_regularizer=None, activity_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, input_dim=None, **kwargs):
+        self.init = initializations.get(init)
         self.activation = activations.get(activation)
         self.output_dim = output_dim
-
-        self.initial_weights = weights
-
         self.input_dim = input_dim
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.initial_weights = weights
+        self.input_spec = [InputSpec(ndim='2+')]
+
         if self.input_dim:
             kwargs['input_shape'] = (self.input_dim,)
-        self.input = K.placeholder(ndim=2)
         super(LinDense, self).__init__(**kwargs)
 
-    def build(self):
-        input_dim = self.input_shape[1]
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1]
+        self.input_dim = input_dim
+        self.input_spec = [InputSpec(dtype=K.floatx(),
+                                     ndim='2+')]
 
-        self.W = self.init((input_dim, self.output_dim))
-        self.b = K.zeros((self.output_dim,))
-        self.non_trainable_weights = [self.W, self.b]
+        self.W = self.add_weight((input_dim, self.output_dim),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight((self.output_dim,),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+
         self.trainable = False
+        self.built = True
 
-    @property
-    def output_shape(self):
-        return (self.input_shape[0], self.output_dim)
+    def call(self, x, mask=None):
+        output = K.dot(x, self.W)
+        if self.bias:
+            output += self.b
+        return self.activation(output)
 
-    def get_output(self, train=False):
-        X = self.get_input(train)
-        output = self.activation(K.dot(X, self.W) + self.b)
-        return output
+    def get_output_shape_for(self, input_shape):
+        assert input_shape and len(input_shape) >= 2
+        assert input_shape[-1] and input_shape[-1] == self.input_dim
+        output_shape = list(input_shape)
+        output_shape[-1] = self.output_dim
+        return tuple(output_shape)
 
     def get_config(self):
-        config = {'name': self.__class__.__name__,
-                  'output_dim': self.output_dim,
+        config = {'output_dim': self.output_dim,
                   'init': self.init.__name__,
                   'activation': self.activation.__name__,
+                  'W_regularizer': self.W_regularizer.get_config() if self.W_regularizer else None,
+                  'b_regularizer': self.b_regularizer.get_config() if self.b_regularizer else None,
+                  'activity_regularizer': self.activity_regularizer.get_config() if self.activity_regularizer else None,
+                  'W_constraint': self.W_constraint.get_config() if self.W_constraint else None,
+                  'b_constraint': self.b_constraint.get_config() if self.b_constraint else None,
+                  'bias': self.bias,
                   'input_dim': self.input_dim}
         base_config = super(LinDense, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-class LinDenseTrainable(Layer):
-    '''Just your regular fully connected NN layer.
-
-    # Input shape
-        2D tensor with shape: `(nb_samples, input_dim)`.
-
-    # Output shape
-        2D tensor with shape: `(nb_samples, output_dim)`.
-
-    # Arguments
-        output_dim: int > 0.
-        init: name of initialization function for the weights of the layer
-            (see [initializations](../initializations.md)),
-            or alternatively, Theano function to use for weights
-            initialization. This parameter is only relevant
-            if you don't pass a `weights` argument.
-        activation: name of activation function to use
-            (see [activations](../activations.md)),
-            or alternatively, elementwise Theano function.
-            If you don't specify anything, no activation is applied
-            (ie. "linear" activation: a(x) = x).
-        weights: list of numpy arrays to set as initial weights.
-            The list should have 1 element, of shape `(input_dim, output_dim)`.
-        W_regularizer: instance of [WeightRegularizer](../regularizers.md)
-            (eg. L1 or L2 regularization), applied to the main weights matrix.
-        b_regularizer: instance of [WeightRegularizer](../regularizers.md),
-            applied to the bias.
-        activity_regularizer: instance of [ActivityRegularizer](../regularizers.md),
-            applied to the network output.
-        W_constraint: instance of the [constraints](../constraints.md) module
-            (eg. maxnorm, nonneg), applied to the main weights matrix.
-        b_constraint: instance of the [constraints](../constraints.md) module,
-            applied to the bias.
-        input_dim: dimensionality of the input (integer).
-            This argument (or alternatively, the keyword argument `input_shape`)
-            is required when using this layer as the first layer in a model.
-    '''
-    input_ndim = 2
-
-    def __init__(self, output_dim, init='identity_vstacked', activation='linear', weights=None,
-                 input_dim=None, **kwargs):
-
-        self.init = initializations_scol.get(init)
-        self.activation = activations.get(activation)
-        self.output_dim = output_dim
-
-        self.initial_weights = weights
-
-        self.input_dim = input_dim
-        if self.input_dim:
-            kwargs['input_shape'] = (self.input_dim,)
-        self.input = K.placeholder(ndim=2)
-        super(LinDenseTrainable, self).__init__(**kwargs)
-
-    def build(self):
-        input_dim = self.input_shape[1]
-
-        self.W = self.init((input_dim, self.output_dim))
-        self.b = K.zeros((self.output_dim,))
-        self.non_trainable_weights = [self.W, self.b]
-        self.trainable = True
-
-    @property
-    def output_shape(self):
-        return (self.input_shape[0], self.output_dim)
-
-    def get_output(self, train=False):
-        X = self.get_input(train)
-        output = self.activation(K.dot(X, self.W) + self.b)
-        return output
-
-    def get_config(self):
-        config = {'name': self.__class__.__name__,
-                  'output_dim': self.output_dim,
-                  'init': self.init.__name__,
-                  'activation': self.activation.__name__,
-                  'input_dim': self.input_dim}
-        base_config = super(LinDenseTrainable, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
