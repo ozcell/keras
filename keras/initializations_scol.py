625fcae9db1086ab9fa802415515563952e637b2
ozcell <ozsel@ozsel-mac.local>
2017-01-22 00:14:00 -0500

    Keras update issues

--- keras/initializations_scol.py
+++ keras/initializations_scol.py
@@ -3,12 +3,13 @@ import numpy as np
 from . import backend as K
 from .utils.generic_utils import get_from_module
 
-def identity_vstacked(shape, scale=1):
+def identity_vstacked(shape, scale=1, name=None, dim_ordering='th'):
     scale = shape[1]/shape[0]
     a = np.identity(shape[1])
     for i in range(1, int(1/scale)):
         a = np.vstack((a, np.identity(shape[1])))
-    return K.variable(a)
+    return K.variable(a, name=name)
 
-def get(identifier):
-    return get_from_module(identifier, globals(), 'initialization')
+def get(identifier, **kwargs):
+    return get_from_module(identifier, globals(),
+                           'initialization', kwargs=kwargs)        
